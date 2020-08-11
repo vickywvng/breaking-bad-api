23e6eba526a8f89a0ee71958973ab09651ceb923
Vicky Wang <vickywang@Vickys-MacBook-Pro.local>
2020-08-11 14:30:15 -0400

    add breaking bad project

---
+++ src/components/ui/Header.js
@@ -0,0 +1,13 @@
+// rafce
+import React from 'react';
+import logo from '../../img/logo.png';
+
+const Header = () => {
+    return (
+        <header className='center'>
+            <img src={logo} alt='' />
+        </header>
+    )
+}
+
+export default Header
